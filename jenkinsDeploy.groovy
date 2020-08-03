1c78dd75e32b5d840416e15098f3f990960c48e5
EC2 Default User <ec2-user@ip-172-31-11-17.ec2.internal>
2020-08-03 03:05:21 +0000

    Added deploy job

---
+++ jenkinsDeploy.groovy
@@ -0,0 +1,10 @@
+properties([
+    parameters([
+        booleanParam(defaultValue: false, description: 'Please select to apply the changes.', name: 'terraformApply'), 
+        booleanParam(defaultValue: false, description: 'Please select to destroy the changes.', name: 'terraformDestroy'), 
+        booleanParam(defaultValue: false, description: 'Please select to run the job in debug mode.', name: 'debugMode'), 
+        choice(choices: ['dev', 'qa', 'stage', 'prod'], description: 'Please select the environment to deploy.', name: 'environment')
+    ])
+])
+
+println('Hello World')
